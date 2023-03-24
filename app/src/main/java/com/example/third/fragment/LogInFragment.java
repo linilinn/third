44741d0c6575837c841f7516fa81746a49128563
linilinn <125601952+linilinn@users.noreply.github.com>
2023-03-25 00:06:58 +0300

    second task

--- app/src/main/java/com/example/third/fragment/LogInFragment.java
+++ app/src/main/java/com/example/third/fragment/LogInFragment.java
@@ -1,5 +1,13 @@
 package com.example.third.fragment;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.third.R;
@@ -7,5 +15,82 @@ import com.example.third.R;
 public class LogInFragment extends Fragment {
     public LogInFragment() {
         super(R.layout.log_in_fragment);
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(getContext(), "onCreate", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onCreate");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onCreateView");
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onViewCreated");
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onViewStateRestored");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Toast.makeText(getContext(), "onStart", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Toast.makeText(getContext(), "onResume", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Toast.makeText(getContext(), "onPause", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Toast.makeText(getContext(), "onStop", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onStop");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onSaveInstanceState");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getContext(), "onDestroy", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onDestroy");
     }
 }
