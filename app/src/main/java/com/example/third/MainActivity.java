daff756c238dfd715ee79ba907a2bb903d4256d0
linilinn <125601952+linilinn@users.noreply.github.com>
2023-03-25 11:03:27 +0300

    third task

--- app/src/main/java/com/example/third/MainActivity.java
+++ app/src/main/java/com/example/third/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.third;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.third.fragment.LogInFragment;
+import com.example.third.fragment.StartFragment;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
-                    .add(R.id.log_in_fragment, LogInFragment.class, null)
+                    .add(R.id.main_fragment, StartFragment.class, null)
                     .commit();
         }
     }
--- app/src/main/java/com/example/third/fragment/StartFragment.java
+++ app/src/main/java/com/example/third/fragment/StartFragment.java
@@ -1,11 +1,63 @@
 package com.example.third.fragment;
 
-import androidx.fragment.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.third.MainActivity;
 import com.example.third.R;
+import com.example.third.databinding.StartFragmentBinding;
 
 public class StartFragment extends Fragment {
+    FragmentManager fragmentManager;
+    StartFragmentBinding binding;
+    MainActivity mainActivity;
     public StartFragment() {
         super(R.layout.start_fragment);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fragmentManager = getParentFragmentManager();
+        fragmentManager.setFragmentResultListener("switch to StartFragment", this, (requestKey, result) -> {
+            String results = result.getString("resultText");
+            Toast.makeText(getContext(), results + ", приложение пока не готово", Toast.LENGTH_LONG).show();
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = StartFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.button.setOnClickListener(view1 -> {
+            Bundle result = new Bundle();
+            result.putString("resultText", binding.editTextTextPersonName.getText().toString());
+            fragmentManager.setFragmentResult("switch to LogInFragment", result);
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.main_fragment, new LogInFragment());
+            fragmentTransaction.addToBackStack(null).commit();
+        });
     }
 }
